77cb6939662164ad46d9b2fc84f60bd43554f741
Arseniy <arseniy.burtsev@gmail.com>
2023-03-03 23:24:47 +0300

    Added login page

--- tortik/lib/main.dart
+++ tortik/lib/main.dart
@@ -3,6 +3,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:tortik/pages/start.dart';
 import 'package:tortik/pages/home.dart';
+import 'package:tortik/pages/login.dart';
 
 void main() => runApp(MaterialApp(
   debugShowCheckedModeBanner: false,
@@ -10,5 +11,8 @@ void main() => runApp(MaterialApp(
   routes: {
     '/': (context) => const Start(),
     '/home': (context) => const Home(),
+    '/loginPage': (context) => const LoginPage(),
+    '/register': (context) => const Register(),
+    '/login': (context) => const Login(),
   },
 ));
