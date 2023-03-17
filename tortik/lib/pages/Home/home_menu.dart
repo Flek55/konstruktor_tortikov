473653dc5498fca4f3f850a9ff3c11e2963fbc10
Arseniy <arseniy.burtsev@gmail.com>
2023-03-17 18:25:15 +0300

    Minor code changes

--- tortik/lib/pages/Home/home_menu.dart
+++ tortik/lib/pages/Home/home_menu.dart
@@ -14,10 +14,21 @@ class _HomeMenuState extends State<HomeMenu> {
       backgroundColor: Colors.white,
       body: Column(
         children: [
-          const Padding(padding: EdgeInsets.only(top: 50)),
+          const Padding(padding: EdgeInsets.only(top: 70)),
           Row(
-            children: const [],
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              const Padding(padding: EdgeInsets.only(left: 40)),
+              IconButton(onPressed: () {
+
+              },
+                  icon: const Icon(Icons.account_circle, size: 40,),
+                  padding: EdgeInsets.zero,),
+              const Padding(padding: EdgeInsets.only(left: 80)),
+              const Text("Adress", style: TextStyle(fontSize: 15, fontFamily: 'Roboto'),),//TODO: найти виджет и сделать карту
+            ],
           ),
+          const Padding(padding: EdgeInsets.only(top: 50)),
           Row(children: const [
             Padding(padding: EdgeInsets.only(top: 50, left: 40)),
             Text('Отличный кофе\nВсегда и везде!',
